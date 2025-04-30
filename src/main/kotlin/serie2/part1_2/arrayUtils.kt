f375867569fecbb01f16c017540819e4625aa387
Simão Silva <A51773@alunos.isel.pt>
2025-04-30 15:28:34 +0100

    arrayUtils done and TestsPassed;

--- src/main/kotlin/serie2/part1_2/arrayUtils.kt
+++ src/main/kotlin/serie2/part1_2/arrayUtils.kt
@@ -1,5 +1,19 @@
 package serie2.part1_2
 
+// Como estamos a trabalhar num maxHeap, o menor elemento não pode estar nos nós internos pois são maiores que os valores que estão abaixo.
+// Temos então de procurar nessas folhas
+// As folhas começam no índice heapSize / 2  pois o maior valor é o do primeiro indice.
+//Percorremos apenas os elementos folha (da metade em diante) para encontrar o menor.
+
 fun minimum(maxHeap: Array<Int>, heapSize: Int): Int {
-    TODO("Not yet implemented")
+
+    val firtLeaf = heapSize / 2
+    var min = maxHeap[firtLeaf]
+
+    for(i in firtLeaf + 1 until heapSize) {
+        if(maxHeap[i] < min) {
+            min = maxHeap[i]
+        }
+    }
+    return min
 }
