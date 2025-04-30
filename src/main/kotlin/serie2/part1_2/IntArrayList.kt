0c2f0b6391495d43bebbfd28b32255e8f19b678d
Miguel Ângelo <elkazimir1337@hotmail.com>
2025-04-30 09:27:00 +0100

    IntArrayList Added

--- src/main/kotlin/serie2/part1_2/IntArrayList.kt
+++ src/main/kotlin/serie2/part1_2/IntArrayList.kt
@@ -1,24 +1,73 @@
 package serie2.part1_2
 
-class IntArrayList : Iterable <Int> {
+class IntArrayList: Iterable<Int> {
 
-    fun append(x:Int):Boolean {
-        TODO("Not yet implemented")
+    private var list: Array<Int?>
+    var size: Int = 0
+
+    constructor(size: Int) {
+        list = arrayOfNulls<Int?>(size)
     }
 
-    fun get(n:Int):Int?  {
-        TODO("Not yet implemented")
+    private fun increaseCapacity() {
+        val newArray = arrayOfNulls<Int?>(2 * list.size)
+        // arraycopy(src, srcStart, dst, dstStart, size)
+        System.arraycopy(list, 0, newArray, 0, size)
+        list = newArray
+
     }
 
-    fun addToAll(x:Int)   {
-        TODO("Not yet implemented")
+    fun append(x: Int): Boolean {
+        var counter = size
+        if (x in list) return false
+        if (size == list.size) {
+           increaseCapacity()
+       }
+            list[size++] = x
+            counter++
+            return true
     }
 
-    fun remove():Boolean {
-        TODO("Not yet implemented")
+    fun get(n: Int): Int?  {
+        return if (n in list.indices) list[n] else null
+    }
+
+    fun addToAll(x: Int) {
+        for (i in 0 until size) {
+            if (list[i] != null)
+                list[i] = list[i]!! + x
+        }
+    }
+
+    fun remove(): Boolean {
+        if (list.isEmpty()) return false
+        else {
+            list[0] = null
+            size--
+            if (size < 0) return false
+            else {
+                System.arraycopy(list, 0, list, 0, size)
+                return true
+            }
+        }
     }
 
     override fun iterator(): Iterator<Int> { // Opcional
         TODO("Not yet implemented")
     }
+}
+
+fun main() {
+    val list = IntArrayList(3)
+    println(list.append(10))
+    println(list.append(4))
+    println(list.get(0))
+    println(list.get(1))
+    println(list.get(0))
+    println(list.get(1))
+    println(list.addToAll(10))
+    println(list.get(0))
+    println(list.get(1))
+    println(list.remove())
+    println(list.get(0))
 }
--- src/main/kotlin/serie2/part1_2/arrayUtils.kt
+++ src/main/kotlin/serie2/part1_2/arrayUtils.kt
@@ -1,5 +1,4 @@
 package serie2.part1_2
-import kotlin.random.Random
 
 fun minimum(maxHeap: Array<Int>, heapSize: Int): Int {
     TODO("Not yet implemented")
